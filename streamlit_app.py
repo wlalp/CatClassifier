98cb802f3709cbbb7db88f5488f636fc20e87898
wlalp <98447309+wlalp@users.noreply.github.com>
2024-03-25 20:27:09 +0000

    Implemented selector box, added CatClassifier_v01 to available models, and updated dataset page.

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,21 +10,38 @@ from keras.preprocessing.image import img_to_array, load_img, array_to_img
 st.set_page_config("Prediction Page",":cat:")
 st.markdown("<h1 style='text-align: center; color: white;'>Cat Breed Classifier</h1>", unsafe_allow_html=True)
 model_name = "CatClassifier_v0"
+model_file = "models/CatClassifier_v0.keras"
+
 name_html = f"<p style='text-align: center; color: grey;font-family: Courier New'>Currently using [{model_name}]</p>"
-
-st.markdown(name_html, unsafe_allow_html=True)
-
-file_col, breed_col = st.columns(2)
+name_holder = st.empty()
+name_holder.markdown(name_html,unsafe_allow_html=True)
 
 
-class_names = sorted(['bombay', 'bengal', 'persian', 'ragdoll', 'sphynx', 'russian_blue', 'egyptian_mau',
+file_col, breed_col,model_col = st.columns(3)
+
+def update_html(model_name):
+    name_html = f"<p style='text-align: center; color: grey;font-family: Courier New'>Currently using [{model_name}]</p>"
+    name_holder.markdown(name_html,unsafe_allow_html=True)
+
+models = ("CatClassifier_v0","CatClassifier_v01")
+
+with model_col:
+    model_name = st.selectbox("Select a model",models)
+    model_file = "models/"+ model_name + ".keras"
+
+if model_name == "CatClassifier_v0":
+    class_names = sorted(['bombay', 'bengal', 'persian', 'ragdoll', 'sphynx', 'russian_blue', 'egyptian_mau',
                'siamese', 'birman', 'abyssinian', 'british_shorthair', 'maine_coon'])
+else:
+    class_names = sorted(['bombay', 'bengal', 'persian', 'ragdoll', 'sphynx', 'russian_blue', 'egyptian_mau',
+               'siamese', 'birman', 'abyssinian', 'british_shorthair', 'maine_coon','domestic_shorthair'])
 breed_col.header("Supported breeds")
+
 for cl in class_names:
     breed_col.write(cl)
 
-model_name = "CatClassifier_v0"
-model_file = "CatClassifier_v0.keras"
+
+
 model = keras.models.load_model(model_file, compile=False)
 
 pred_data = pd.DataFrame(columns=["Breed","Confidence"])
