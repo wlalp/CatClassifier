955bf0afdb8ebaa3784e42f356b716545de175e6
wlalp <98447309+wlalp@users.noreply.github.com>
2024-04-22 06:59:13 +0000

    Removed deprecated heatmap function.

--- pages/3_Prediction_Information.py
+++ pages/3_Prediction_Information.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 
 
 
-last_conv_layers = {'CatClassifier_v01':'conv2d_3','CatClassifier_v01':'conv2d_3', 'VGG16V_6':''}
+last_conv_layers = {'CatClassifier_v01':'conv2d_3','CatClassifier_v01':'conv2d_3', 'VGG16V_6':'conv2d_1'}
 
 
 try:
@@ -19,39 +19,10 @@ try:
 except:
     st.write("No prediction found! Please input an image on the main page to see your results here.")
 
-#Reference: https://keras.io/examples/vision/grad_cam/
-def heatmap(img,model,last_conv_layer):
-    img_array = keras.utils.img_to_array(img)
-    img_array = np.expand_dims(img,axis=0)
 
-    grad_model = keras.models.Model(
-        model.inputs, [model.get_layer(last_conv_layer).output, model.output]
-    )
-    
-    with tf.GradientTape() as tape:
-        last_conv_layer_output, preds = grad_model(img_array)
-        if pred_index is None:
-            pred_index = tf.argmax(preds[0])
-        class_channel = preds[:, pred_index]
-
-    grads = tape.gradient(class_channel, last_conv_layer_output)
-
-    pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
-
-    last_conv_layer_output = last_conv_layer_output[0]
-    heatmap = last_conv_layer_output @ pooled_grads[..., tf.newaxis]
-    heatmap = tf.squeeze(heatmap)
-
-    heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
-    return heatmap.numpy()
-
-
-if 'model' in st.session_state and 'img' in st.session_state:
+if 'model' in st.session_state and 'img' in st.session_state and 'model_name' in st.session_state:
     st.write("Let's take a look at what the model saw!")
 
     model = st.session_state.model
     model_name = st.session_state.model_name
     img = st.session_state.img
-    heat = heatmap(img,model, last_conv_layers[model_name])
-
-    st.pyplot(heat)
