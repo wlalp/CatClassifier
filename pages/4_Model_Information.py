6238bacaef62061004c6152d18c21f576aeb0d32
wlalp <98447309+wlalp@users.noreply.github.com>
2024-04-22 07:23:16 +0000

    Removed stray comment.

--- pages/4_Model_Information.py
+++ pages/4_Model_Information.py
@@ -5,7 +5,6 @@ try:
     model_name = st.session_state.model_name
     model = st.session_state.model
     st.markdown(f"<h1 style='text-align: center;'>You are using {model_name}!</h1>", unsafe_allow_html=True)
-#    st.write(f"{model_name} Summary")
     with st.expander(f"{model_name} Summary"):
         model.summary(print_fn=st.write,expand_nested=True)
     
