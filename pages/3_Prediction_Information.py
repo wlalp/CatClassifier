56af0d4c23f2b9e61fff66bbe48b3d68b0f443e1
wlalp <98447309+wlalp@users.noreply.github.com>
2024-04-22 07:22:22 +0000

    Added relevant links for predicted breed.

--- pages/3_Prediction_Information.py
+++ pages/3_Prediction_Information.py
@@ -8,6 +8,23 @@ import matplotlib.pyplot as plt
 
 
 last_conv_layers = {'CatClassifier_v01':'conv2d_3','CatClassifier_v01':'conv2d_3', 'VGG16V_6':'conv2d_1'}
+breed_links = ['https://www.purina.com/cats/cat-breeds/abyssinian-cat',
+               'https://www.purina.com/cats/cat-breeds/bengal-cat',
+               'https://www.purina.com/cats/cat-breeds/birman-cat',
+               'https://www.purina.com/cats/cat-breeds/bombay',
+               'https://www.purina.com/cats/cat-breeds/british-shorthair',
+               'https://www.purina.com/cats/cat-breeds/american-shorthair',
+               'https://www.purina.com/cats/cat-breeds/egyptian-mau',
+               'https://www.purina.com/cats/cat-breeds/maine-coon',
+               'https://www.purina.com/cats/cat-breeds/persian',
+               'https://www.purina.com/cats/cat-breeds/ragdoll',
+               'https://www.purina.com/cats/cat-breeds/russian-blue',
+               'https://www.purina.com/cats/cat-breeds/siamese',
+               'https://www.purina.com/cats/cat-breeds/sphynx'
+               ]
+
+def init_links(links):
+    {}
 
 
 try:
@@ -26,3 +43,16 @@ if 'model' in st.session_state and 'img' in st.session_state and 'model_name' in
     model = st.session_state.model
     model_name = st.session_state.model_name
     img = st.session_state.img
+
+    class_names = sorted(['Bombay', 'Bengal', 'Persian', 'Ragdoll', 'Sphynx', 'Russian Blue', 'Egyptian Mau',
+                'Siamese', 'Birman', 'Abyssinian', 'British Shorthair', 'Maine Coon','Domestic Shorthair'])
+    
+    link_index = 0
+    link_dict = dict()
+    for c in class_names:
+        link_dict[c] = breed_links[link_index]
+        link_index += 1
+
+    st.write("More information: ",link_dict[pred_breed])
+    
+    
